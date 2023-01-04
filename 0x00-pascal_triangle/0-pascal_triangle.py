fd23790c597a52873512d15fce15fa508f480311
eyasuyid <eyasuyid@gmail.com>
2023-01-04 14:28:45 +0300

    my pascal fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Module makes a pasical triagle"""
+"""Module makes a pasical triagle
+"""
 
 
-def pascal_triangle(n):
-    """Function return n rows of pasical triangle """
+def pascal_tri(n):
+    """Function return n rows of pasical tri
+    """
     if n <= 0:
         return []
-    check_list = [[1]]
+    tri_list = [[1]]
     for j in range(n - 1):
-        check_list.append([1] + [check_list[j][i] + check_list[j][i + 1]
-                    for i in range(len(check_list[j]) - 1)] + [1])
-    return check_list
+        tri_list.append([1] + [tri_list[j][i] + tri_list[j][i + 1]
+                        for i in range(len(tri_list[j]) - 1)] + [1])
+    return tri_list
